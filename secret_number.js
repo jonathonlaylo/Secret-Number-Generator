b5c1c20ab5c039b90124d79596100091c0da7a02
jonathon.laylo@gmail.com <jonathon.laylo@gmail.com>
2016-11-12 13:58:08 -1000

    secret number generator completed

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,27 @@
 'use strict';
 module.exports = function() {
-  
+
+  var num = Math.random()*1000000;
+  return function mySecretNumber(){
+    return num;
+  };
 };
+
+//======================================
+
+// const secret = require('secret_number.js')
+
+
+// var myNum = secret(); // generates random #, returns: function mysecretNumber
+
+// myNum(); //123
+// myNum(); //123
+
+// var myNum2 = secret();
+
+// myNum2(); //567
+// myNum2(); //567
+
+// myNum1(); //123
+
+
